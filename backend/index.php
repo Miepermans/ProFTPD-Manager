d8863a15e0b69a4f5f18b9eff72abd688190b351
Miepermans <miep@pixelbar.nl>
2013-02-04 16:08:03 +0100

    Added a expiry system

---
+++ backend/index.php
@@ -0,0 +1,43 @@
+<?php
+
+include "../include/dbinfo.inc.php";
+include "../include/config.inc.php";
+
+mysql_connect($aDBInfo['address'],$aDBInfo['username'],$aDBInfo['password']) or die(mysql_error());
+mysql_select_db($aDBInfo['database']);
+
+function ClearDeletedUsers() {
+
+        $query_fillusers = "SELECT id, deleted FROM ftpuser";
+        $query_hosts = mysql_query($query_fillusers)
+        or die(mysql_error());
+
+        while ($row = mysql_fetch_row($query_hosts)){
+
+          if ($row[1] == "1") {
+            mysql_query("DELETE FROM `ftpuser` WHERE `id` = '$row[0]'")
+            or die(mysql_error());
+          } else {
+          }
+        }    
+}
+
+function ClearInvalidUsers() {
+
+        $query_fillusers = "SELECT id, validtill FROM ftpuser";
+        $query_hosts = mysql_query($query_fillusers)
+        or die(mysql_error());
+
+        while ($row = mysql_fetch_row($query_hosts)){
+          if ($row[1] == "0000-00-00 00:00:00") {
+          } else {
+            mysql_query("DELETE FROM `ftpuser` WHERE `id` = '$row[0]' AND validtill < curDate() ")
+            or die(mysql_error());
+          }
+        }
+}
+
+ClearDeletedUsers();
+ClearInvalidUsers();
+
+?>
--- frontend/index.php
+++ frontend/index.php
@@ -33,13 +33,22 @@ if(isset($username)) {
   $homedir = $config['path'].$username;
 }
 
+if (isset($_POST['valid'])) {
+  $valid = $_POST['valid'];
+} 
+
 $uid = $config['uid'];
 $gid = $config['gid'];
 $shell = $config['shell'];
 
 if(isset($username) && isset($password)) {
-  mysql_query("INSERT INTO `ftpuser` (`userid`,`passwd`,`uid`,`gid`,`homedir`,`shell`) VALUES('$username','$password','$uid','$gid','$homedir','$shell')")
-  or die(mysql_error());
+  if ($valid == "0") {
+    mysql_query("INSERT INTO `ftpuser` (`userid`,`passwd`,`uid`,`gid`,`homedir`,`shell`,`validtill`) VALUES('$username','$password','$uid','$gid','$homedir','$shell','0000-00-00 00:00:00')")
+    or die(mysql_error());
+  } else {
+    mysql_query("INSERT INTO `ftpuser` (`userid`,`passwd`,`uid`,`gid`,`homedir`,`shell`,`validtill`) VALUES('$username','$password','$uid','$gid','$homedir','$shell', CURRENT_DATE + INTERVAL $valid WEEK)")
+    or die(mysql_error());
+  }
 }
 
 if(isset($_POST['id'])) {
@@ -63,23 +72,31 @@ echo '
               <th>Username</th>
               <th>Password</th>
               <th>Home Directory</th>
-              <th>delete</th>
+              <th>Account valid till</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>';
 
-        $query_fillusers = "SELECT id, userid, passwd, homedir, deleted FROM ftpuser";
+        $query_fillusers = "SELECT id, userid, passwd, homedir, validtill, deleted FROM ftpuser";
         $query_hosts = mysql_query($query_fillusers)
         or die(mysql_error());
 
         while ($row = mysql_fetch_row($query_hosts)){
 
-          if ($row[4] == "1") {
+          if ($row[5] == "1") {
           } else {
+            if ($row[4] == "0000-00-00 00:00:00") {
+              $dt = "Unlimited";
+            } else {
+              $dt = new DateTime($row[4]);
+              $dt = $dt->format('Y-M-d');
+            }
             echo "<tr>";
             echo "<td>"; echo $row[1]; echo "</td>";
             echo "<td>"; echo $row[2]; echo "</td>";
             echo "<td>"; echo $row[3]; echo "</td>";
+            echo "<td>"; echo $dt; echo "</td>";
             echo "<td><label class=checkbox><input name=id[] value=$row[0] type=checkbox></label</td>";
             echo "</tr>";
           }
@@ -87,11 +104,19 @@ echo '
 
 
         echo '
+          <tbody>
           <tr>
           <td><input name=username type=text value=username></input></td>
           <td><input name=password type=text value='; echo rand_string(8); echo'></input></td>
+          <td><select name=valid>
+            <option value=1 >1 Week valid</option>
+            <option value=2 >2 Weeks valid</option>
+            <option value=4 selected>4 Weeks valid</option>
+            <option value=0 >Unlimited valid</option>
+            </select>
+          </td>
           </tr>
-        <tbody>
+        
         </table>
       <input type="submit">
       </form>
